d4033f097b0f660e7223eb288985bfaff657db7d
Filipe <123255512+F-i-l-i-p-e@users.noreply.github.com>
2023-08-17 18:51:11 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using GameDb.Models; // Models Namespace
+using Microsoft.AspNetCore.Mvc; // MVC Namespace
+using System.Diagnostics; // Diagnostics Namespace
+
+namespace GameDb.Controllers // Controllers Namespace
+{
+    public class HomeController : Controller // Home Controller
+    {
+        private readonly ILogger<HomeController> _logger; // Logger
+
+        public HomeController(ILogger<HomeController> logger) // Constructor
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() // Index Action
+        {
+            return View();
+        }
+
+        public IActionResult Privacy() // Privacy Action
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)] // Cache Attribute
+        public IActionResult Error() // Error Action
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
